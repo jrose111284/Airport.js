973bab47ccb07ce13e4b8023a4e4c25f2747b0cf
jamierose <jrose111284@gmail.com>
2016-02-08 10:48:44 +0000

    can clear planes for takeoff

--- src/Plane.js
+++ src/Plane.js
@@ -2,7 +2,13 @@
 
 function Plane(){
 
-}
+};
+
 Plane.prototype.land = function(airport){
   airport.clearForLanding(this);
+  this._landing = airport;
+};
+
+Plane.prototype.takeoff = function(airport){
+  this._landing.clearForTakeOff('');
 };
